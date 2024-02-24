6403cbe929208bed21dc87a0dc0d95576e2a9073
Edwin Shi <edwinshi1021@hotmail.com>
2024-02-23 23:56:30 -0800

    update

---
+++ src/Arrays/asList/Main.java
@@ -0,0 +1,16 @@
+package asList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] arr = {"a", "b", "c"};
+
+        // mutable: cannot add or remove elements from the list
+        List<String> list = Arrays.asList(arr);
+        list.set(0, "e");
+
+        System.out.println(list);
+    }
+}
---
+++ src/Arrays/sort/Main.java
@@ -0,0 +1,12 @@
+package sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = { 3, 2, 4, 1, 5};
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
