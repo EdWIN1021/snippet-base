d172e67a573012629ab3d43663e95b7efa9335c4
Edwin Shi <edwinshi1021@hotmail.com>
2024-03-21 22:12:06 -0700

    update

---
+++ src/collections/Set/HashSet/retainAll/Main.java
@@ -0,0 +1,15 @@
+package collections.Set.HashSet.retainAll;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> mySet1 = new HashSet<>(Arrays.asList(1,1,2,2,3,3));
+        Set<Integer> mySet2 = new HashSet<>(Arrays.asList(1,2,2,2,4,4));
+
+        mySet1.retainAll(mySet2);
+        System.out.println(mySet1);
+    }
+}
