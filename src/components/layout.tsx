37d8e319a99d09eb6567991006327969c5348790
Edwin Shi <edwinshi1021@hotmail.com>
2024-09-23 16:07:27 -0700

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,12 +7,26 @@ import {
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/shadcn/style.css";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import SideBar from "./side-bar";
-import CustomBlockNoteViews from "./custom-block-note-view";
+// import CustomBlockNoteViews from "./custom-block-note-view";
+
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function Layout() {
   const [data, setData] = useState();
@@ -35,6 +49,44 @@ export default function Layout() {
     getData();
   }, []);
 
+  const initialNodes = useMemo(
+    () => [
+      { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+      {
+        id: "2",
+        position: { x: 0, y: 100 },
+        style: {
+          backgroundColor: "#6865A5",
+          width: "500px",
+          height: "500px",
+          textAlign: "left",
+        },
+        data: {
+          label: (
+            <div>
+              <SyntaxHighlighter language="go" style={atomOneDark}>
+                {data ? atob(data) : "No content available"}
+              </SyntaxHighlighter>
+            </div>
+          ),
+        },
+      },
+    ],
+    [data]
+  );
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  useEffect(() => {
+    setNodes(initialNodes);
+  }, [initialNodes, setNodes]);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
     <ResizablePanelGroup direction="horizontal" className="min-h-[100vh]">
       <ResizablePanel defaultSize={20}>
@@ -42,7 +94,7 @@ export default function Layout() {
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel defaultSize={80}>
-        {loading ? (
+        {/* {loading ? (
           <div className="flex h-full items-center justify-center">
             <span>Loading...</span>
           </div>
@@ -56,7 +108,23 @@ export default function Layout() {
               </SyntaxHighlighter>
             </div>
           </>
-        )}
+        )} */}
+
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+        >
+          <Controls />
+          <MiniMap />
+          <Background variant="dots" gap={12} size={1} />
+        </ReactFlow>
+
+        <SyntaxHighlighter language="go" style={atomOneDark}>
+          {data ? atob(data) : "No content available"}
+        </SyntaxHighlighter>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
