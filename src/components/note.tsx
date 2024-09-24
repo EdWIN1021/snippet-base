b4bfd357929a44cf6f55ea0ceade66b7fb8e751d
Edwin Shi <edwinshi1021@hotmail.com>
2024-09-24 15:55:03 -0700

    update

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router-dom";
+import { ReactFlow } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 const Note = () => {
   const params = useParams();
-  return <div>note {params.noteId}</div>;
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div>{params.noteId}</div>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  );
 };
 
 export default Note;
