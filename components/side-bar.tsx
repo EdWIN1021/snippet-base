44b9aa324485c7dd02b9bc7fc3c0d4e1988300fb
Edwin Shi <edwinshi1021@hotmail.com>
2024-09-24 19:40:23 -0700

    update

---
+++ components/side-bar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const Sidebar = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default Sidebar;
